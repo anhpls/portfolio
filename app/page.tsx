bccc27bc6a8605621432d914d951756f8c78b432
Anh Huynh <anhpls@Mac.lan>
2025-02-09 11:23:40 -0800

    mailing x2

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ const navItems = [
     icon: <IconBrandLinkedin />,
     href: "https://www.linkedin.com/in/anhpls/",
   },
-  { title: "Contact", icon: <IconMail />, href: "mailto:anh.h.duy@gmail.com" },
+  { title: "Contact", icon: <IconMail />, href: "anh.h.duy@gmail.com" },
 ];
 
 export default function Home() {
